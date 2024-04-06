b2eabbb87c96bb24c0de8c021e9689e5df19f899
MatheusVict <matheusvictorhenrique@gmail.com>
2024-04-06 07:37:45 -0300

    feat: basic live list card
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card'; 
 
 @Component({
   selector: 'app-live-list',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, MatCardModule],
   templateUrl: './live-list.component.html',
   styleUrl: './live-list.component.css'
 })
