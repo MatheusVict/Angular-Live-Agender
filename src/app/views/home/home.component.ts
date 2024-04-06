a1eaaf6b7ceaff9c0857b424ee27f067be3491fa
MatheusVict <matheusvictorhenrique@gmail.com>
2024-04-06 06:50:33 -0300

    feat: add live list component
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button'
+import { LiveListComponent } from './live-list/live-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, LiveListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
