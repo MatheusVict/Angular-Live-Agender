c27e3bcfd099a2a71d16ddfabbdb14fa76b6960f
MatheusVict <matheusvictorhenrique@gmail.com>
2024-04-07 20:01:09 -0300

    feat: lazy loading for home
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './views/home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', loadComponent: () => import('./views/home/home.component').then(m => m.HomeComponent)}
 ];
