314143e66250213cfdfdb73c122b0c9310d39020
MatheusVict <matheusvictorhenrique@gmail.com>
2024-04-07 19:29:15 -0300

    feat: loading snnip
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'
 import { LiveListComponent } from './live-list/live-list.component';
 import { MatDialog } from '@angular/material/dialog';
 import { LiveFormDialogComponent } from './live-form-dialog/live-form-dialog.component';
@@ -16,7 +16,7 @@ import { MatNativeDateModule } from '@angular/material/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog) { }
 
   addLive(): void {
     const dialogRef = this.dialog.open(LiveFormDialogComponent, {
@@ -25,7 +25,7 @@ export class HomeComponent {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      
+
     });
   }
 }
--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -7,17 +7,20 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { LocalDateTimePipe } from '../../../shared/pipe/local-date-time.pipe';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatDialogModule} from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-live-list',
   standalone: true,
-  imports: [MatTabsModule, MatCardModule, MatChipsModule, LocalDateTimePipe, MatDialogModule],
+  imports: [MatTabsModule, MatCardModule, MatChipsModule, LocalDateTimePipe, MatDialogModule, MatProgressBarModule],
   templateUrl: './live-list.component.html',
   styleUrl: './live-list.component.css'
 })
 export class LiveListComponent implements OnInit {
   livesPrevious: Live[] = [];
   nextLives: Live[] = [];
+  next: boolean = false;
+  previous: boolean = false;
 
   constructor(private liveService: LiveService, private sanitizer: DomSanitizer) { }
 
@@ -32,6 +35,8 @@ export class LiveListComponent implements OnInit {
       this.livesPrevious.forEach(live => {
         live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
       })
+
+      this.previous = true;
     });
 
     this.liveService.getLivesWithFlag('next').subscribe(data => {
@@ -40,6 +45,8 @@ export class LiveListComponent implements OnInit {
         live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
       })
       console.log(this.nextLives)
+
+      this.next = true;
     });
   }
 
